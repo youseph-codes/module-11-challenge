4d12a5a7282b04437c6bd7ea6ee168cb0c2d35a4
Yousopp <yousephagueye@gmail.com>
2022-10-25 21:29:09 -0400

    updated server.js file, added variables

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const PORT = process.env.PORT || 1357;
+const application = express();
+const apiRoutes = require('./Develop/routes/apiRoutes');
+const htmlROUTES = require('./Develop/routes/htmlRoutes');
+
+application.use()
+
+application.use()
+
+application.use()
+
+application.use()
+
+application.use()
+
+application.listen()
